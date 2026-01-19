4f6fe10a76f279fd973e2495d39dd6f43c2daa6c
doyoung <jads77@naver.com>
2026-01-19 21:42:36 +0900

    r1

--- exam_10.c
+++ exam_10.c
@@ -3,7 +3,7 @@
 int main(void){
     int year = 0;
 
-    printf("년도를 입력하세요. : ");
+    printf("ddd년도를 입력하세요. : ");
     scanf("%d",&year);
 
     if (((year%4==0)&&(year%100!=0))||(year%400==0)) printf("%d 년은 윤년입니다.\n",year);
