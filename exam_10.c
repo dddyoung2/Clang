9d1e47f8d70544dd48ff37557a1b8e047437e202
doyoung <jads77@naver.com>
2026-01-19 20:28:11 +0900

    first Clang File

---
+++ exam_10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int year = 0;
+
+    printf("년도를 입력하세요. : ");
+    scanf("%d",&year);
+
+    if (((year%4==0)&&(year%100!=0))||(year%400==0)) printf("%d 년은 윤년입니다.\n",year);
+    else printf("윤년아니네요.\n");
+
+    return 0;
+}
